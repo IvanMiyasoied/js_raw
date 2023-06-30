2380bcb193344fc16ea145646d7b83d7494fd81d
IvanMiyasoied <121316974+IvanMiyasoied@users.noreply.github.com>
2023-06-30 10:49:35 +0300

    new begining

---
+++ app.js
@@ -0,0 +1,59 @@
+// let cpuVersion = 145.4
+// let softwareVersion = 23.45
+// let updateVersion = 1.234
+
+
+
+
+
+// const mainCore = {
+//     getMainInfo (kerbin,cpuVersion) {
+//        return kerbin.name + ' ' + cpuVersion;
+//     },
+//     getAdditionalInfo(softwareVersion) {
+//        return softwareVersion 
+//     },
+//     updateRobot(kerbin,updateVersion) {
+//       return kerbin.name + ' ' + 'robot is updated to' + ' ' + updateVersion
+//     },
+//   };
+  
+//   const navigationCore = {
+//     getCoords (kerbin) {
+//       return 'x = ' + kerbin.coords.x + ' ' + 'y = ' + kerbin.coords.y;
+//     },
+//     __proto__:mainCore
+//   };
+  
+//   const movementCore = {
+//     setTargetCoords (kerbin) {
+
+//     },
+//     __proto__:navigationCore
+//   };
+
+//   const kerbin = {
+//     name: "Kerbin",
+//     coords: {
+//       x:3,
+//       y:2
+//     },
+//     target: {
+//       coords:{
+//         x:2,
+//         y:3
+//       }
+//     },
+//     __proto__:movementCore
+//   };
+//   mainCore.getMainInfo(kerbin,cpuVersion);
+  
+//   console.log(kerbin.getCoords);
+//   console.log(kerbin.updateRobot);
+//   console.log(kerbin.getAdditionalInfo);
+//   console.log(kerbin.getMainInfo);
+
+  
+
+  
+  
